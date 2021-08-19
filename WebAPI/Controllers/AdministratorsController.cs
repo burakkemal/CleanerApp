dcca25c0b9490f9ee8400392319d52665eb9a14b
Burak Kemal Koyuncu <77545526+burakkemal@users.noreply.github.com>
2021-08-19 18:21:02 +0300

    AdministratorsController added.

---
+++ WebAPI/Controllers/AdministratorsController.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdministratorsController : ControllerBase
+    {
+        IAdministratorService _administratorService;
+
+        public AdministratorsController(IAdministratorService administratorService)
+        {
+            _administratorService = administratorService;
+        }
+
+        [HttpGet("getAll")]
+        public IActionResult GetAll()
+        {
+            var result = _administratorService.GetAll();
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Administrator administrator)
+        {
+            var result = _administratorService.Add(administrator);
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+        [HttpPut("update")]
+        public IActionResult Update(Administrator administrator)
+        {
+            var result = _administratorService.Update(administrator);
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+    }
+}
